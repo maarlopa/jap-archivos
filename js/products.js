f88db2762c09b8ca3ca4d434033669526c1bdc4d
maarlopa <88385764+maarlopa@users.noreply.github.com>
2021-08-11 20:15:46 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -5,4 +5,30 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
 });
 
-getJSONData("https://japdevdep.github.io/ecommerce-api/product/all.json");
+var listadoProductos = [];
+function mostrarProductos(){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            
+            showCategoriesListIndividual(listadoProductos);
+        }
+        
+    });
+}
+function showCategoriesListIndividual(array){
+   
+    let htmlContentToAppend = `<a href="">Vovler atras</a>`;
+    for(let i = 0; i < array.length; i++){
+        let category = array[i];
+        htmlContentToAppend += 
+        `<div class="list-group-item list-group-item-action"><div class="row"><div class="col-3"><img src="` + 
+        category.imgSrc + `" alt="` + category.desc +
+         `" class="img-thumbnail"> </img></div><div class="col"><div class="d-flex w-100 justify-content-between"><h4 class="mb-1">`+
+          category.name +`</h4><small class="text-muted">` + category.currency + ` ` + category.cost + 
+          `</small></div><p>` + category.description + `</p></div></div></div>`
+        }
+    document.getElementById("container").innerHTML = htmlContentToAppend;
+    
+}
