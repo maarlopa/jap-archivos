24fa8ec84052d8a086826d587075ffe7510cabaa
maarlopa <88385764+maarlopa@users.noreply.github.com>
2021-08-11 09:33:16 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,6 +1,6 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function ("https://japdevdep.github.io/ecommerce-api/product/all.json") {
+document.addEventListener("DOMContentLoaded", function (https://japdevdep.github.io/ecommerce-api/product/all.json) {
 
 });
