7532b339b67ec04169e8bbbe2c023a4f17bd9d44
maarlopa <88385764+maarlopa@users.noreply.github.com>
2021-08-12 12:05:33 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -2,22 +2,26 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    
+    async function desplegarProductos(PRODUCTS_URL){
+        let promise = await fetch(PRODUCTS_URL);
+        let array = await promise.json();
 
+        let productos = "";
+        for (let index=0; index < array.length; index++){
+        let nombre = array[index].name;
+        let descripcion = array[index].description;
+        let costo =  array[index].cost;
+        let moneda =  array[index].currency;
+        let imagen = array[index].imgSrc;
+        let contador_ventas = array[index].soldCount;
+        productos += `<p>${nombre}</p><p>${descripcion}</p><p>${costo}</p><p>${moneda}</p><p>${contador_ventas}</p> <img src =${imagen}`
+        }
+        document.getElementById('listado-productos').innerHTML = productos;
+    }
+
+    desplegarProductos();
 });
+   
 
-function agregarProductosAListado(array){
-  let productos = "";
-  for (let index=0; index < array.length; index++){
-  let nombre = array[index].name;
-  let descripcion = array[index].description;
-  let costo =  array[index].cost;
-  let moneda =  array[index].currency;
-  let imagen = array[index].imgSrc;
-  let contador_ventas = array[index].soldCount;
-
-  productos += `<p>${nombre}</p><p>${descripcion}</p><p>${costo}</p><p>${moneda}</p><p>${contador_ventas}</p> <img src =${imagen}>`
- }
-  document.getElementById('listado-productos').innerHTML = productos;
-}
-
-agregarProductosAListado();
+  
