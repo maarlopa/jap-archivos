d29da3ee2bd336446dda01dd791b23a424de57f1
maarlopa <88385764+maarlopa@users.noreply.github.com>
2021-08-11 21:02:21 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -5,30 +5,27 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
 });
 
-var listadoProductos = [];
 function mostrarProductos(){
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
-            categoriesArray = resultObj.data;
-            
+    getJSONData(PRODUCTS_URL).then(function(resultadoFetch){
+        if (resultadoFetch.status === "ok"){
             agregarProductosAListado(listadoProductos);
         }
         
     });
 }
 function agregarProductosAListado(array){
-   
-    let htmlContentToAppend = `<a href="">Vovler atras</a>`;
-    for(let i = 0; i < array.length; i++){
-        let category = array[i];
-        htmlContentToAppend += 
-        `<div class="list-group-item list-group-item-action"><div class="row"><div class="col-3"><img src="` + 
-        category.imgSrc + `" alt="` + category.desc +
-         `" class="img-thumbnail"> </img></div><div class="col"><div class="d-flex w-100 justify-content-between"><h4 class="mb-1">`+
-          category.name +`</h4><small class="text-muted">` + category.currency + ` ` + category.cost + 
-          `</small></div><p>` + category.description + `</p></div></div></div>`
-        }
-    document.getElementById("container").innerHTML = htmlContentToAppend;
-    
+  let productos = "";
+  for (let index=0; index < array.length; index++){
+  let nombre = array[index].name;
+  let descripcion = array[index].description;
+  let costo =  array[index].cost;
+  let moneda =  array[index].currency;
+  let imagen = array[index].imgSrc;
+  let contador_ventas = array[index].soldCount;
+
+  productos += `<p>${nombre}</p><p>${descripcion}</p><p>${costo}</p><p>${moneda}</p><p>${contador_ventas}</p> <img src =${imagen}>`
+ }
+  document.getElementById('contenido').innerHTML = productos;
 }
+
+mostrarProductos();
